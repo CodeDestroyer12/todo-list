05ff94ca856b9a621a0d07fadc957764fb1a4e3f
codedestroyer12 <sajan12dhakal@gmail.com>
2020-05-10 19:53:18 +0545

    Updates

---
+++ precache-manifest.fd6e6279f9ae8c592406ea4d5f6b5233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fd443a7d8e0ea4e1d551f16f0a194f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "7ecaf60e605a3936725f",
+    "url": "/todo-list/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e9d89b10f5c09a68c63b",
+    "url": "/todo-list/static/css/main.a34cb505.chunk.css"
+  },
+  {
+    "revision": "7ecaf60e605a3936725f",
+    "url": "/todo-list/static/js/2.bbae46ab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-list/static/js/2.bbae46ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d89b10f5c09a68c63b",
+    "url": "/todo-list/static/js/main.c6ac0309.chunk.js"
+  },
+  {
+    "revision": "e35d169da06fded0b1d1",
+    "url": "/todo-list/static/js/runtime-main.7b98a2f6.js"
+  }
+]);
